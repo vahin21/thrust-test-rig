0416709fa6e1190378996cf629fa5532385af014
Vahin <73296301+vahin21@users.noreply.github.com>
2026-03-13 19:04:56 +0530

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,31 +1,17 @@
 import mqtt from "mqtt";
 
-let latestValue = 0;
-let calibrationValue = 0;
 let lastUpdate = 0;
 
 const client = mqtt.connect("mqtt://broker.hivemq.com");
 
 client.on("connect", () => {
-  client.subscribe("thrustrig/loadcell1");
-  client.subscribe("thrustrig/cal_result");
+  client.subscribe("thrustrig/heartbeat");
 });
 
-client.on("message", (topic, message) => {
-  if (topic === "thrustrig/loadcell1") {
-    latestValue = parseFloat(message.toString());
-    lastUpdate = Date.now();
-  }
-
-  if (topic === "thrustrig/cal_result") {
-    calibrationValue = message.toString();
-  }
+client.on("message", () => {
+  lastUpdate = Date.now();
 });
 
 export default function handler(req, res) {
-  res.status(200).json({
-    thrust: latestValue,
-    calibration: calibrationValue,
-    lastUpdate
-  });
+  res.status(200).json({ lastUpdate });
 }
