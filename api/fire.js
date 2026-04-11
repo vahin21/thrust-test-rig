e19ad44adecb7b9b86ee54dd96c4bcf5ea350370
Vahin <73296301+vahin21@users.noreply.github.com>
2026-04-11 20:16:53 +0530

    Update fire.js

--- api/fire.js
+++ api/fire.js
@@ -1,16 +1,12 @@
 import mqtt from "mqtt";
 
-const client = mqtt.connect("mqtt://broker.hivemq.com");
-
-client.on("connect", () => {
-  client.subscribe("thrustrig/loadcell1");
-});
-
 export default function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
 
-  client.publish("thrustrig/fire", "ON");
-  setTimeout(() => client.publish("thrustrig/fire", "OFF"), 10000);
-
-  res.status(200).json({ status: "Ignition triggered" });
+  const client = mqtt.connect("mqtt://broker.hivemq.com");
+  client.on("connect", () => {
+    client.publish("thrustrig/fire", "ON");
+    client.end();
+    res.status(200).json({ status: "Fired" });
+  });
 }
